f1a2e0446038dd910b9ff26338148ecf91ebf38b
OlenaAndrushchenko <olena.anru.anru@gmail.com>
2024-09-10 04:02:49 +0200

    created store model with its methods

--- src/main/java/dev/olena/models/Store.java
+++ src/main/java/dev/olena/models/Store.java
@@ -1,5 +1,39 @@
 package dev.olena.models;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Store {
-    
+    private String storeNumber;
+    private String storeOwner;
+    private String storeTaxIdentifier;
+    private List<Computer> computers;
+
+    public Store(String storeNumber, String storeOwner, String storeTaxIdentifier) {
+        this.storeNumber = storeNumber;
+        this.storeOwner = storeOwner;
+        this.storeTaxIdentifier = storeTaxIdentifier;
+        this.computers = new ArrayList<>();
+    }
+
+    public void addComputer(Computer computer) {
+        computers.add(computer);
+    }
+
+    public void removeComputerByBrand(String brand) {
+        computers = computers.stream().filter(computer -> !computer.getBrand().equalsIgnoreCase(brand))
+        .collect(Collectors.toList());
+    }
+
+    public Computer findComputerByBrand(String brand) {
+        return computers.stream()
+                .filter(computer -> computer.getBrand().equalsIgnoreCase(brand))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<Computer> getAllComputers() {
+        return new ArrayList<>(computers);
+    }
 }
